2c692923c57422f5d7361f96fa367cf3b3af9d60
Mariah McLaughlin <hello@mariahmclaughlin.com>
2017-11-25 11:39:35 -0500

    testing without db

--- app.py
+++ app.py
@@ -7,22 +7,22 @@ import urllib.parse
 #define app
 app = Flask(__name__)
 
-#DB information
-conn = "mongodb://" +  admin + ":" + urllib.parse.quote_plus(pw) + "@ds121456.mlab.com:21456/heroku_63785bfl"
+# #DB information
+# conn = "mongodb://" +  admin + ":" + urllib.parse.quote_plus(pw) + "@ds121456.mlab.com:21456/heroku_63785bfl"
 
-client = pymongo.MongoClient(conn)
-db = client.heroku_63785bfl
+# client = pymongo.MongoClient(conn)
+# db = client.heroku_63785bfl
 
 @app.route("/")
 def index():
 
     return render_template("index.html")
 
-@app.route("/api/v1/")
-def api():
-    schools = list(db.items.find())
+# @app.route("/api/v1/")
+# def api():
+#     schools = list(db.items.find())
 
-    return json.dumps(schools, sort_keys=True, indent=4, default=json_util.default)
+#     return json.dumps(schools, sort_keys=True, indent=4, default=json_util.default)
     
-if __name__ == "__main__":
-    app.run(debug=False)
+# if __name__ == "__main__":
+#     app.run(debug=False)
