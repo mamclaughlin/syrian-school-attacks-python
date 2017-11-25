b89ff68f105270ac8004e329f7874c80932e9308
Mariah McLaughlin <hello@mariahmclaughlin.com>
2017-11-25 12:40:31 -0500

    edit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 import pymongo
 import json
 from bson import json_util, ObjectId
-from keys import admin, pw
+import keys
 import urllib.parse
 #define app
 app = Flask(__name__)
